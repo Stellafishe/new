855e7084d2bd04bad4129d6b5fbb17e65250a8b2
Stellafishe <stellafishes@gmail.com>
2023-06-13 14:34:19 -0500

    new commit

--- new/ContentView.swift
+++ new/ContentView.swift
@@ -7,20 +7,81 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var selectedRating = 0
+    @State var genderNeutral : Bool
+    @State var wheelChairAccessible : Bool
+    @State var babyChangingStations : Bool
+    @State private var review = ""
+    @State private var textTitle = ""
     var body: some View {
+        
+        
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+      Spacer()
+            
+            Text("Rating")
+                .font(.headline)
+            HStack {
+                ForEach(1...5, id: \.self) { index in
+                    Button(action: {
+                        selectedRating = index
+                    }) {
+                        Image(systemName: index <= selectedRating ? "star.fill" : "star")
+                            .foregroundColor(index <= selectedRating ? .yellow : .gray)
+                    }
+                }
+            }
+            
+            
+            Toggle(isOn: $genderNeutral) {
+                Text("Gender Neutral")
+            }
+            Toggle(isOn: $wheelChairAccessible) {
+                Text("Wheel Chair Accessible")
+            }
+            Toggle(isOn: $babyChangingStations) {
+                Text("Baby Changing Stations")
+            }
+            
+            
+            //            ZStack{
+            //                background(:_alignment:)
+            Spacer()
+            
+            Text(textTitle)
+                .font(.title)
+                .multilineTextAlignment(.center)
+            
+            TextField("comment", text: $review)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.cyan, width: 1)
+                .padding()
+            
+            Button("Submit") {
+                textTitle = "\(review)"
+            }
+            .font(.title2)
+            .foregroundColor(Color.purple)
+            .buttonStyle(.bordered)
+            .tint(.orange)
+            
+            
+            //            }
         }
-        .padding()
+        
     }
-}
+        }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(genderNeutral: false, wheelChairAccessible:  false,babyChangingStations: false)
     }
 }
+
--- new/newApp.swift
+++ new/newApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct newApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(genderNeutral: false, wheelChairAccessible:  false,babyChangingStations: false)
         }
     }
 }
